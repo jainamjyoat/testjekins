5e341330423c40b00120dfd330dcbd4df4d08ba0
jainamjyoat <jainamjyoat2005@gmail.com>
2026-06-05 10:13:44 +0530

    hi

---
+++ src/main/java/app.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Jenkins CI/CD Project ");
+        System.out.println("=================================");
+
+        System.out.println("Build Successful");
+        System.out.println("Application Running Successfully");
+    }
+}
